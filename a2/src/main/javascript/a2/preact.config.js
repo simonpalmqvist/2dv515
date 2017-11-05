30f28fe39eeb2cb57666bf810c3820929ad73ce1
Simon Palmqvist <simon.palmqvist@gmail.com>
2017-11-05 19:01:07 +0100

    Changing so proxy is only for dev

--- a2/src/main/javascript/a2/preact.config.js
+++ a2/src/main/javascript/a2/preact.config.js
@@ -1,10 +1,12 @@
-module.exports = function(config) {
-    config.devServer.proxy = [
-        {
-            path: '/api/**',
-            target: 'http://localhost:8080',
-            changeOrigin: true,
-            changeHost: true,
-        }
-    ];
+module.exports = function(config, env) {
+    if(!env.production) {
+        config.devServer.proxy = [
+            {
+                path: '/api/**',
+                target: 'http://localhost:8080',
+                changeOrigin: true,
+                changeHost: true,
+            }
+        ];
+    }
 };
